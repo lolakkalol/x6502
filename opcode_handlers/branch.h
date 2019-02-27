eabe005eb6d89c2c3c7402972e31c6d81c868a84
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2019-02-26 23:19:57 -0500

    Correct implementation of BBS and BBR #oops

--- opcode_handlers/branch.h
+++ opcode_handlers/branch.h
@@ -59,113 +59,113 @@ case BRA:
     break;
 
 case BBR0:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x01)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x01)) {
         branch_offset = s1;
     }
     break;
 
 case BBR1:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x02)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x02)) {
         branch_offset = s1;
     }
     break;
 
 case BBR2:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x04)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x04)) {
         branch_offset = s1;
     }
     break;
 
 case BBR3:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x08)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x08)) {
         branch_offset = s1;
     }
     break;
 
 case BBR4:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x10)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x10)) {
         branch_offset = s1;
     }
     break;
 
 case BBR5:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x20)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x20)) {
         branch_offset = s1;
     }
     break;
 
 case BBR6:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x40)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x40)) {
         branch_offset = s1;
     }
     break;
 
 case BBR7:
-    s1 = NEXT_BYTE(m);
-    if(!(s1 & 0x80)) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(!(m->mem[r1] & 0x80)) {
         branch_offset = s1;
     }
     break;
 
 case BBS0:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x01) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x01) {
         branch_offset = s1;
     }
     break;
 
 case BBS1:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x20) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x02) {
         branch_offset = s1;
     }
     break;
 
 case BBS2:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x04) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x04) {
         branch_offset = s1;
     }
     break;
 
 case BBS3:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x08) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x08) {
         branch_offset = s1;
     }
     break;
 
 case BBS4:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x01) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x10) {
         branch_offset = s1;
     }
     break;
 
 case BBS5:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x20) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x20) {
         branch_offset = s1;
     }
     break;
 
 case BBS6:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x40) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x40) {
         branch_offset = s1;
     }
     break;
 
 case BBS7:
-    s1 = NEXT_BYTE(m);
-    if(s1 & 0x80) {
+    r1 = ZP(NEXT_BYTE(m));
+    if(m->mem[r1] & 0x80) {
         branch_offset = s1;
     }
     break;
